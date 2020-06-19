9cde0573ac42171fa0ab60931a65ce21b212b52f
Felix Chan <felixchan29@gmail.com>
2020-06-19 10:18:01 -0400

    testing promise instead of async

--- controllers/art.js
+++ controllers/art.js
@@ -2,36 +2,77 @@ const Art = require('../models/artModel')
 
 // @desc   Get all from Art
 // @route  GET /api/v1/Art
-exports.getArt = async (req, res, next) => {
-    try {
-      const art = await Art.find();
-      return res.status(200).json({
-        success: true,
-        data: art
+// exports.getArt = async (req, res, next) => {
+//     try {
+//       const art = await Art.find();
+//       return res.status(200).json({
+//         success: true,
+//         data: art
+//       });
+//     } catch (error) {
+//       res.sendStatus(500).json({
+//         success: false,
+//         error: 'Server error'
+//       })
+//     }
+// }
+
+//testing
+exports.getArt = (req, res, next) => {
+
+      Art.find((err, art) => {
+          if (err) {
+            console.log(err)
+            res.sendStatus(500).json({
+                success: false,
+                error: 'Server error'
+              })
+          }
+          else {
+            return res.status(200).json({
+                success: true,
+                data: art
+              });
+          }
       });
-    } catch (error) {
-      res.sendStatus(500).json({
-        success: false,
-        error: 'Server error'
-      })
-    }
+
 }
   
 // @desc   Add to Art
 // @route  POST /api/v1/Art
-exports.addArt = async (req, res, next) => {
-    try {
-      const art = await Art.create(req.body);
-      return res.status(201).json({
-        success: true,
-        data: art
-      });
-    } catch (error) {
-      res.sendStatus(500).json({
-        success: false,
-        error: 'Server error'
-      })
-    }
+// exports.addArt = async (req, res, next) => {
+//     try {
+//       const art = await Art.create(req.body);
+//       return res.status(201).json({
+//         success: true,
+//         data: art
+//       });
+//     } catch (error) {
+//       res.sendStatus(500).json({
+//         success: false,
+//         error: 'Server error'
+//       })
+//     }
+// }
+//testing
+exports.addArt = (req, res, next) => {
+    const art = new Art(req.body)
+
+    art.save()
+        .then(art => {
+            res.status(201).json({
+                success: true,
+                data: art
+                })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                success: false,
+                error: 'failed to add new art item'
+            })
+        })
+    
 }
   
 // @desc   Update Art
