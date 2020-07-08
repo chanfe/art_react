63792f38edc605d0a88d854db7dae0fad89b0adc
Felix Chan <felixchan29@gmail.com>
2020-07-07 20:24:32 -0400

    added piture frame for the app and added temp br for spaceing

--- client/src/components/Picture.js
+++ client/src/components/Picture.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import defaultpicture from "../assets/Default_Image_Thumbnail.png"
 import './Picture.scss'
 
-import { Container, Dimmer, Loader, Segment, Image } from 'semantic-ui-react'
+import { Container, Dimmer, Loader, Segment, Image} from 'semantic-ui-react'
 
 const renderLoadingPicture = () => {
   return (
@@ -24,14 +24,12 @@ const renderPicture = (data) => {
   return (
     <Container>
       <h1>{data.title}</h1>
-      <div class="frame">
-        <div class="mat">
-          <div class="art">
-            <img src={data.imageUrl} alt={data.title} />
-          </div>
-        </div>
-      </div>
-      {/* <Image src={data.imageUrl} fluid /> */}
+      <br />
+
+      <img id="picture" src={data.imageUrl} alt={data.title} />
+      <br />
+      <br />
+      <br />
       {data.description? <p>{data.description}</p>: <div></div>}
       {data.creator? <p>by {data.creator}</p>: <div></div>}
     </Container>
