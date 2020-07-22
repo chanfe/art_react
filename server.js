f58351cf6b9593b27ea0d8409f576be6df09d31a
Felix Chan <felixchan29@gmail.com>
2020-07-22 10:23:18 -0400

    init heroku commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const path = require("path")
+
 const cors = require('cors')
 
 dotenv.config({ path: './config/config.env' });
@@ -21,9 +23,21 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Uses the builted client html page (frontend)
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 // API Routes
 app.use('/api/v1/art', art);
 app.use('/api/v1/reaction', reaction);
+
+// if api is not being used then it use the client 
+if (process.env.NODE_ENV === 'production') {           
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Express server
 const PORT = process.env.PORT || 5000;
